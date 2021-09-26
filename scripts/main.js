cc246a186aa8158ec3094493c22568b05e1f344b
merzm <merzm@rose-hulman.edu>
2021-09-26 15:53:26 -0400

    portfolio done (I think)

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+function main() {
+    console.log("reeady");
+
+    document.querySelector("#docker").onclick = (event) => {
+        collapse(document.querySelector("#docker"));
+    };
+
+    document.querySelector("#thesis").onclick = (event) => {
+        collapse(document.querySelector("#thesis"));
+    };
+
+    document.querySelector("#gitlab").onclick = (event) => {
+        collapse(document.querySelector("#gitlab"));
+    };
+
+    document.querySelector("#processor").onclick = (event) => {
+        collapse(document.querySelector("#processor"));
+    };
+
+}
+
+function collapse(label) {
+        sibling = label.nextElementSibling;
+        if (sibling.style.display === "block") {
+            sibling.style.display = "none";
+            label.innerHTML = label.innerHTML.replace("(collapse)", "(expand)");
+        } else {
+            sibling.style.display = "block";
+            label.innerHTML = label.innerHTML.replace("(expand)", "(collapse)");
+        }
+}
+
+
+main();
