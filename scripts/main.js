9d8650a87b0e94b88bd553458b20be9033c7a1d3
merzm <merzm@rose-hulman.edu>
2021-10-01 16:39:17 -0400

    Cleaning up and adding detail

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
 function main() {
     console.log("reeady");
 
-    document.querySelector("#docker").onclick = (event) => {
-        collapse(document.querySelector("#docker"));
-    };
+    // document.querySelector("#docker").onclick = (event) => {
+    //     collapse(document.querySelector("#docker"));
+    // };
 
     document.querySelector("#thesis").onclick = (event) => {
         collapse(document.querySelector("#thesis"));
     };
 
-    document.querySelector("#gitlab").onclick = (event) => {
-        collapse(document.querySelector("#gitlab"));
-    };
+    // document.querySelector("#gitlab").onclick = (event) => {
+    //     collapse(document.querySelector("#gitlab"));
+    // };
 
     document.querySelector("#processor").onclick = (event) => {
         collapse(document.querySelector("#processor"));
